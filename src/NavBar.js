bec446631ef4d5366b16844aeb39ee31425f74ab
Daisy Yi <daisyyi@rocketmail.com>
2023-01-10 14:29:40 -0700

    add styling to NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,24 +1,65 @@
-src/NavBar.jsimport React from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
 function NavBar() {
     return (
-        <div>
-            <NavLink to="/">
-                Home
+        <NavStyle>
+            <li><NavLink exact to="/">
+                <p>Home</p>
             </NavLink>
-            <NavLink to="/about">
-                About
-            </NavLink>
-            <NavLink to="/PostList">
-                Notes
-            </NavLink>
-            <NavLink to="/submitpost">
-                Submit a Post 
-            </NavLink>
-
-        </div>
+            </li>
+            <li><NavLink to="/about">
+                <p>About</p>
+            </NavLink></li>
+            <li><NavLink to="/PostList">
+                <p>Notes</p>
+            </NavLink></li>
+            <li className="submit"><NavLink to="/submitpost">
+                <p>Submit a Post</p>
+            </NavLink></li>
+        </NavStyle>
     )
 }
 
 export default NavBar;
+
+
+const NavStyle = styled.ul`
+list-style-type: none;
+margin: 10px;
+padding: 0;
+overflow: hidden;
+background-color: #333;
+li {
+    border-right: 1px solid #bbb;
+  }
+li:last-child {
+    border-right: none;
+  }
+li {
+    float: left;
+  }
+  
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+  }
+  
+  /* Change the link color to #111 (black) on hover */
+li a:hover {
+    background-color: #111;
+  }
+.active {
+    background-color: #87CEEB;
+    color:white;
+    text-shadow: 1px 1px #000000;
+  }
+.submit {
+    float:right;
+    border-left: 1px solid #bbb;
+  }
+`
