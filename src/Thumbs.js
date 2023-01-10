7a98cb9e4fb98f3156b60f91985a449f866f15c3
Daisy Yi <daisyyi@rocketmail.com>
2023-01-10 14:47:36 -0700

    style Thumbs.js

--- src/Thumbs.js
+++ src/Thumbs.js
@@ -1,15 +1,30 @@
 import React from "react";
 import { GoThumbsup, GoThumbsdown } from "react-icons/go";
+import styled from "styled-components";
  
 function Thumbs( {down, up, handleDown, handleUp} ) {
 
  
    return(
-       <div>
-           <button onClick={() => handleUp(up+1)}>{up}  <GoThumbsup /></button>
-           <button onClick={() => handleDown(down+1)}>{down} <GoThumbsdown /></button>
-       </div>
+       <ThumbStyle>
+           <button className="left" onClick={() => handleUp(up+1)}> {up}  <GoThumbsup /></button>
+           <button onClick={() => handleDown(down+1)}> {down} <GoThumbsdown /></button>
+       </ThumbStyle>
    )
 }
  
 export default Thumbs;
+
+const ThumbStyle = styled.div`
+button{
+    background-color: #42ddf5
+    color: white;
+    height: 35px;
+    width: 50px;
+    font-size: 30px
+    margin: 100px;
+}
+.left(
+    margin-right: 10px;
+)
+`
