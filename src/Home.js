ac2529131f75885e59c63aa4eb81071f1cc060fe
Daisy Yi <daisyyi@rocketmail.com>
2023-01-12 21:34:09 -0700

    cut unnecessary code

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
-import About from "./About";
-import PostList from "./PostList";
 import styled from "styled-components";
 
 function Home() {
     return (
         <HomeStyle>
+            <div>
             <img src="https://i.imgur.com/kyblMxa.jpg"></img>
+            <h3 className="text">Southwest Fieldnotes</h3>
+            </div>
 
         </HomeStyle>
     )
@@ -19,5 +20,16 @@ img{
     max-width: 100%;
     height: auto;
     opacity: .75;
+}
+.text{
+    position:absolute;
+    top: 55%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: white;
+    text-shadow: 2px 2px 5px black;
+    font-size: 90px;
+    font-family: 'Times New Roman', serif;
+    font-style: oblique;
 }
 `
