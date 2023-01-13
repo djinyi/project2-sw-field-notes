02d17aa64717ff0d711b7a57f7e07ee618498eb2
Daisy Yi <daisyyi@rocketmail.com>
2023-01-12 17:16:30 -0700

    change header style

--- src/PostList.js
+++ src/PostList.js
@@ -48,11 +48,10 @@ export default PostList;
 
 const PostStyle = styled.div`
 h2{
-    font-size: 30px;
+    font-size: 40px;
     text-align: center;
-    padding-bottom: 20px;
-    border-bottom-style: solid;
-    border-bottom-color: rgba(135, 206, 235, .2)
+    padding: 25px 45px 25px 45px;
+    background-color: rgba(135, 206, 235, .2);
 }
 `
 
